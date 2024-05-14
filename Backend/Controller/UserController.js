4e237949803f1f94c272a5d8516bc35f511116ca
GatsinziMeek <hackergastinzi@gmail.com>
2024-05-14 23:53:01 +0200

    new add features

--- Backend/Controller/UserController.js
+++ Backend/Controller/UserController.js
@@ -1,11 +1,43 @@
 import asyncHandler from 'express-async-handler';
 
+
+// route POST /api/user/auth
 export const AuthUser = asyncHandler(async (req, res) => {
-    res.status(401);
-    throw new Error('something went');
 
     res.status(200).json({
         message: 'Auth user'
     })
 });
 
+// route POST /api/user
+
+export const RegisterUser = asyncHandler(async (req, res) => {
+    res.status(200).json({
+        message: 'Register user'
+    })
+});
+
+// route POST /api/user/logout
+
+export const LogoutUser = asyncHandler(async (req, res) => {
+    res.status(200).json({
+        message: 'Logout user'
+    })
+});
+
+// route GET /api/user/profile
+export const GettingUserProfile = asyncHandler(async (req, res) => {
+
+    res.status(200).json({
+        message: 'Receive User Profile'
+    })
+});
+
+
+// route PUT /api/user/profile
+export const UpdatedUserProfile = asyncHandler(async (req, res) => {
+
+    res.status(200).json({
+        message: 'Upadate User Profile'
+    })
+});
--- Backend/Routes/UserRoutes.js
+++ Backend/Routes/UserRoutes.js
@@ -1,7 +1,11 @@
 import express from 'express';
-import { AuthUser } from '../Controller/UserController.js';
+import { AuthUser,RegisterUser, LogoutUser, GettingUserProfile, UpdatedUserProfile } from '../Controller/UserController.js';
 const router = express.Router();
 
+router.post('/', RegisterUser);
 router.post('/auth', AuthUser);
+router.post('/logout', LogoutUser);
+router.get('/profile', GettingUserProfile);
+router.put ('/profile', UpdatedUserProfile);
 
 export default router;
